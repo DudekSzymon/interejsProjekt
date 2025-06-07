5c00532e8e632ab97dcc68f3d39e4340908ee021
DudekSzymon <zikos9955@gmail.com>
2025-06-07 20:13:36 +0200

    Projekt

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -7,18 +7,18 @@ export default function LoginPage() {
       <div className="relative hidden lg:block" style={{ backgroundColor: "hsl(0, 0%, 3.9%)" }}>
         <div className="absolute top-6 left-6 z-10 flex items-center">
           <span className="text-white text-2xl mr-2 font-bold">*</span>
-          <span className="text-white text-xl font-medium">Neurevo Med</span>
+          <span className="text-white text-xl font-medium">AutoRent</span>
         </div>
         <div className="absolute bottom-10 left-6 right-6 z-10 flex flex-col text-white">
           <p className="text-xl font-light mb-2">
-            "Sztuczna inteligencja to narzędzie, które przybliży lekarzy do perfekcji"
+            "Nie chodzi o to, dokąd jedziesz, ale czym"
           </p>
           <p className="text-sm font-medium">
-            Prof. Paweł Łęgosz
+            Jeremy Clarkson
           </p>
         </div>
         <img
-          src="images/surgeon.jpeg"
+          src="images/car.jpg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover brightness-[0.4] dark:brightness-[0.2] dark:grayscale" />
       </div>
